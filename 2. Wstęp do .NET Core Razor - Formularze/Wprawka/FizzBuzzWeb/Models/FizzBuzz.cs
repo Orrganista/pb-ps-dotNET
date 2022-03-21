e74a53000d0f354c83e515ee80d4da3eefa76c47
Orrganista <orrganista@gmail.com>
2022-03-21 12:57:55 +0100

    test

--- 2. Wstęp do .NET Core Razor - Formularze/Wprawka/FizzBuzzWeb/Models/FizzBuzz.cs
+++ 2. Wstęp do .NET Core Razor - Formularze/Wprawka/FizzBuzzWeb/Models/FizzBuzz.cs
@@ -4,7 +4,7 @@ namespace FizzBuzzWeb.Models
 {
     public class FizzBuzz
     {
-        [Display(Name = "Twój szczęśliwy numerek")]
+        [Display(Name = "Twój szczęśliwy numere")]
         [Required(ErrorMessage = "Pole jest obowiązkowe"), Range(1, 1000,
         ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
         [FizzBuzzAttribute]
