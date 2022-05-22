e15470608b4cfb584063743cf2784f9d933c04d6
Orrganista <orrganista@gmail.com>
2022-05-22 17:43:36 +0200

    filtrowanie

---
+++ 9. Filtrowanie/Filtrowanko/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Filtrowanko.Utlis;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Filtrowanko.Pages
+{
+    [AddHeaderAttribute("Author", "Rick")]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ 9. Filtrowanie/Filtrowanko/Pages/Privacy.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Filtrowanko.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public override void
+        OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+            int i = 0;
+        }
+        public override void
+        OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            int i = 1;
+        }
+    }
+}
